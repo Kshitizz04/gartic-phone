d6e4728aea8222d4032f33cbb4cefaa3cc4bdc9f
Kshitiz <kshitizs2008@gmail.com>
2024-07-27 16:28:15 +0530

    set up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { Box } from "@mui/material";
+import { useState } from "react";
+import ReactPainter from "react-painter";
+
+import React from 'react'
 
 function App() {
+  const [image, setImage] = useState("")
+
   return (
-    <h1>APP</h1>
+    <>
+      <Box sx={{border: '2px black solid',width:'300px'}}>
+        <ReactPainter
+        onSave={blob=>console.log(blob)}
+          render={({ triggerSave, canvas , imageDownloadUrl}) => (
+            <div>
+              <button onClick={triggerSave} >Save Canvas</button>
+              <button onClick={()=>{setImage(imageDownloadUrl)}}>Submit</button>
+              <div>{canvas}</div>
+            </div>
+          )}
+        />
+      </Box>
+      <img src={image}/>
+    </>
   );
 }
 
